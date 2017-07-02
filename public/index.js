14b5644f9ea022677d48496090535d7dfbd8de31
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-02 21:44:47 +0530

    Add Stream video with RTCPeerConnection

--- public/index.js
+++ public/index.js
@@ -1,27 +1,89 @@
-window.onload = () => {
-  const video = document.getElementById('video')
-  const localURL = URL || window.webkitURL
+let localStream = {}
+let localPeerConnection = {}
+let remotePeerConnection = {}
 
-  navigator.getMedia = navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia
+const localVideo = document.getElementById('localVideo')
+const remoteVideo = document.getElementById('remoteVideo')
 
-  // Constraints
-  const constraints = {
-    video: true,
-    audio: true
-  }
+const startButton = document.getElementById('startButton')
+const callButton = document.getElementById('callButton')
+const hangupButton = document.getElementById('hangupButton')
 
-  const successCB = localMediaStream => {
-    window.stream = localMediaStream
-    video.src = localURL.createObjectURL(localMediaStream)
-  }
-
-  const errorCB = error => {
-    console.log('navigator.getUserMedia error: ', error)
-  }
-
-  // Capture video
-  navigator.getMedia(constraints, successCB, errorCB)
+const trace = text => {
+  console.log((performance.now() / 1000).toFixed(3) + ': ' + text)
 }
+
+startButton.onclick = () => {
+  trace('Requesting local stream')
+  navigator.getUserMedia({audio: true, video: true},
+                          gotStream,
+                          error => {
+                            trace('getUserMedia error: ', error)
+                          })
+}
+
+const gotStream = stream => {
+  trace('Received local stream')
+  localVideo.src = URL.createObjectURL(stream)
+  localStream = stream
+}
+
+callButton.onclick = () => {
+  trace('Starting call')
+  const servers = null
+
+  localPeerConnection = new RTCPeerConnection(servers)
+  trace('Created local peer connection object localPeerConnection')
+  localPeerConnection.onicecandidate = gotLocalIceCandidate
+
+  remotePeerConnection = new RTCPeerConnection(servers)
+  trace('Created remote peer connection object remotePeerConnection')
+  remotePeerConnection.onicecandidate = gotRemoteIceCandidate
+  remotePeerConnection.onaddstream = gotRemoteStream
+
+  localPeerConnection.addStream(localStream)
+  trace('Added localStream to localPeerConnection')
+  localPeerConnection.createOffer(gotLocalDescription,handleError)
+}
+
+const gotLocalIceCandidate = event => {
+  if (event.candidate) {
+    remotePeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate))
+    trace('Local ICE candidate: \n' + event.candidate.candidate)
+  }
+}
+
+const gotRemoteIceCandidate = event => {
+  if (event.candidate) {
+    localPeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate))
+    trace('Remote ICE candidate: \n ' + event.candidate.candidate)
+  }
+}
+
+const gotRemoteStream = event => {
+  remoteVideo.src = URL.createObjectURL(event.stream)
+  trace('Received remote stream')
+}
+
+const gotLocalDescription = description => {
+  localPeerConnection.setLocalDescription(description)
+  trace('Offer from localPeerConnection: \n' + description.sdp)
+  remotePeerConnection.setRemoteDescription(description)
+  remotePeerConnection.createAnswer(gotRemoteDescription, handleError)
+}
+
+const gotRemoteDescription = description => {
+  remotePeerConnection.setLocalDescription(description)
+  trace('Answer from remotePeerConnection: \n' + description.sdp)
+  localPeerConnection.setRemoteDescription(description)
+}
+
+hangupButton.onclick = () => {
+  trace('Ending call')
+  localPeerConnection.close()
+  remotePeerConnection.close()
+  localPeerConnection = null
+  remotePeerConnection = null
+}
+
+const handleError = () => {}
