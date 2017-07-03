80efd95170cd8483b156dfc05aa4115a5391b705
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-03 17:36:16 +0530

    Some intermediate code

--- public/index.js
+++ public/index.js
@@ -1,89 +1,148 @@
-let localStream = {}
-let localPeerConnection = {}
-let remotePeerConnection = {}
+let callToken = '' // unique token for a particular call
+let signalingServer = '' // signalling server for a particular call
+let peerConnection = {} // the actual RTCPeerConnection object, that will be established between 2 users
 
-const localVideo = document.getElementById('localVideo')
-const remoteVideo = document.getElementById('remoteVideo')
+const remoteVideo = document.getElementById('remote_video')
+const localVideo = document.getElementById('local_video')
+const loadingState = document.getElementById('loading_state')
 
-const startButton = document.getElementById('startButton')
-const callButton = document.getElementById('callButton')
-const hangupButton = document.getElementById('hangupButton')
+body.onload = () => {
+  // creates the WebRTC peer connection object
+  peerConnection = new rtcPeerConnection({
+    iceServers: [{'url': 'stun:' + stunServer}] // // stun server info
+  })
 
-const trace = text => {
-  console.log((performance.now() / 1000).toFixed(3) + ': ' + text)
+  // generic handler that sends any ice candidates to the other peer
+  peerConnection.onIceCandidate = iceEvent => {
+    if (iceEvent.candidate) {
+      signalingServer.send(JSON.stringify({
+        type: 'new_ice_candidate',
+        candidate: iceEvent.candidate
+      }))
+    }
+  }
+
+  // display remote video streams when they arrive
+  peerConnection.onaddstream = event => {
+    connectStreamToSrc(event.stream, remoteVideo)
+    loadingState.style.display = 'none'
+    remoteVideo.style.display = 'block'
+  }
+
+  setupVideo()
+
+  if (document.location.hash === '' || document.location.hash === undefined) {
+    // create the unique token for this call
+    let token = Date.now() + '-' + Math.round(Math.random() * 10000)
+    callToken = '#' + token
+    document.location.hash = callToken
+
+    signalingServer.onopen = () => {
+      // setup caller signal handler
+      signalingServer.onmessage = callerSignalHandler
+
+      // tell the signaling server you have joined the call
+      signalingServer.send(
+        JSON.stringify({
+          token: callToken,
+          type: 'join'
+        })
+      )
+    }
+    document.title = 'You are the Caller'
+    loadingState.innerHTML = 'Ready for a call...ask your friend to visit:<br/><br/>' + document.location
+  } else { // you have a hash fragment so you must be the Callee
+    // get the unique token for this call from location.hash
+    callToken = document.location.hash
+
+    signalingServer.onopen = () => {
+      // setup callee signal handler
+      signalingServer.onmessage = calleeSignalHandler
+
+      // tell the signaling server you have joined the call
+      signalingServer.send(
+        JSON.stringify({
+          token: callToken,
+          type: 'join'
+        })
+      )
+    }
+  }
+
+  // setup generic connection to the signaling server using the WebSocket API
+  signalingServer = new Websocket('ws://localhost:3000')
 }
 
-startButton.onclick = () => {
-  trace('Requesting local stream')
-  navigator.getUserMedia({audio: true, video: true},
-                          gotStream,
-                          error => {
-                            trace('getUserMedia error: ', error)
-                          })
-}
+// handle signals as a caller
+const callerSignalHandler = event => {
+  const signal = JSON.parse(event.data)
 
-const gotStream = stream => {
-  trace('Received local stream')
-  localVideo.src = URL.createObjectURL(stream)
-  localStream = stream
-}
-
-callButton.onclick = () => {
-  trace('Starting call')
-  const servers = null
-
-  localPeerConnection = new RTCPeerConnection(servers)
-  trace('Created local peer connection object localPeerConnection')
-  localPeerConnection.onicecandidate = gotLocalIceCandidate
-
-  remotePeerConnection = new RTCPeerConnection(servers)
-  trace('Created remote peer connection object remotePeerConnection')
-  remotePeerConnection.onicecandidate = gotRemoteIceCandidate
-  remotePeerConnection.onaddstream = gotRemoteStream
-
-  localPeerConnection.addStream(localStream)
-  trace('Added localStream to localPeerConnection')
-  localPeerConnection.createOffer(gotLocalDescription,handleError)
-}
-
-const gotLocalIceCandidate = event => {
-  if (event.candidate) {
-    remotePeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate))
-    trace('Local ICE candidate: \n' + event.candidate.candidate)
+  if (signal.type === 'callee_arrived') {
+    peerConnection.createOffer(
+      newDescriptionCreated,
+      logError
+    )
+  } else if (signal.type === 'new_ice_candidate') {
+    peerConnection.addIceCandidate(new RTCIceCandidate(signal.candidate))
+  } else if (signal.type === 'new_description') {
+    peerConnection.setRemoteDescription(new rtcSessionDescription(signal.sdp),
+                                        () => {
+                                          if (peerConnection.remoteDescription.type === 'answer') {
+                                            // extend with your own custom answer handling here
+                                          }
+                                        },
+                                        logError)
+  } else {
+    // extend with your own signal types here
   }
 }
 
-const gotRemoteIceCandidate = event => {
-  if (event.candidate) {
-    localPeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate))
-    trace('Remote ICE candidate: \n ' + event.candidate.candidate)
+// handle signals as a callee
+const calleeSignalHandler = event => {
+  const signal = JSON.parse(event.data)
+  if (signal.type === 'new_ice_candidate') {
+    peerConnection.addIceCandidate(new RTCIceCandidate(signal.candidate))
+  } else if (signal.type === 'new_description') {
+    peerConnection.setRemoteDescription(new rtcSessionDescription(signal.sdp),
+                                        () => {
+                                          if (peerConnection.remoteDescription.type === 'offer') {
+                                            peerConnection.createAnswer(newDescriptionCreated, logError)
+                                          }
+                                        },
+                                        logError)
+  } else {
+    // extend with your own signal types here
   }
 }
 
-const gotRemoteStream = event => {
-  remoteVideo.src = URL.createObjectURL(event.stream)
-  trace('Received remote stream')
+// handler to process new descriptions
+const newDescriptionCreated = description => {
+  peerConnection.setLocalDescription(
+    description,
+    () => {
+      signalingServer.send(
+        JSON.stringify({
+          token: callToken,
+          type: 'new_description',
+          sdp: description
+        })
+      )
+    },
+    logError
+  )
 }
 
-const gotLocalDescription = description => {
-  localPeerConnection.setLocalDescription(description)
-  trace('Offer from localPeerConnection: \n' + description.sdp)
-  remotePeerConnection.setRemoteDescription(description)
-  remotePeerConnection.createAnswer(gotRemoteDescription, handleError)
+const setupVideo = () => {
+  const constraints = {
+    audio: true,
+    video: true
+  }
+  get_user_media(constraints, successCB, logError)
 }
 
-const gotRemoteDescription = description => {
-  remotePeerConnection.setLocalDescription(description)
-  trace('Answer from remotePeerConnection: \n' + description.sdp)
-  localPeerConnection.setRemoteDescription(description)
+const successCB = localStream => {
+  connectStreamToSrc(localStream, localVideo)
+  peerConnection.addStream(localStream)
 }
 
-hangupButton.onclick = () => {
-  trace('Ending call')
-  localPeerConnection.close()
-  remotePeerConnection.close()
-  localPeerConnection = null
-  remotePeerConnection = null
-}
-
-const handleError = () => {}
+const logError = error => console.log(error)
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const path = require('path')
 const server = require('http').createServer(app)
 const io = require('socket.io').listen(server)
 
+let webrtcClients = []
+let webrtcDiscussions = {}
+
 server.listen(process.env.PORT || 3000, () => {
-  console.log('server running')
+  console.log('server running on port 3000')
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
