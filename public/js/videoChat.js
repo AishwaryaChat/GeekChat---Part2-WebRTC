0da71a2655913a2138fc18d96eeba0574c5f14e5
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-04 16:48:12 +0530

    Add code to make a negotiation with calle and send the offer

--- public/js/videoChat.js
+++ public/js/videoChat.js
@@ -4,25 +4,16 @@ const remoteVideo = document.getElementById('remote_video')
 const localVideo = document.getElementById('local_video')
 
 let myPeerConnection = null
+var targetUsername = null
 let myHostname = window.location.hostname
 
 startButton.onclick = () => {
   videoArea.style.display = 'block'
   const constraints = {video: 'true', audio: 'true'}
 
-  createPeerConnection()
+  createPeerConnection() // create a RTCPeerConnection object to make a call
 
   getUserMedia(constraints, gotStream, errorStream)
-}
-
-const createPeerConnection = () => {
-  console.log('inside createPeerConnection, setting up connection')
-  // Create an RTCPeerConnection which knows to use our chosen
-  // STUN server.
-  myPeerConnection = new RTCPeerConnection({iceServers: [{'urls': 'stun:' + stunServer}]})
-
-  myPeerConnection.onicecandidate = handleICECandidateEvent
-  myPeerConnection.onaddstream = handleAddStreamEvent // to add remote stream on local remoteVideo element, one a stream is received
 }
 
 const handleICECandidateEvent = event => {
@@ -40,6 +31,22 @@ const handleAddStreamEvent = event => {
   connectStreamToSrc(event.stream, remoteVideo)
 }
 
+const handleNegotiationEvent = event => {
+  myPeerConnection.createOffer()
+  .then(offer => {
+    console.log('offer', offer)
+  })
+  .then(() => {
+    sendToServer({
+      name: userName.id,
+      target: selectedUser,
+      type: 'video-offer',
+      sdp: myPeerConnection.localDescription
+    })
+  })
+  .catch(reportError)
+}
+
 const gotStream = stream => {
   connectStreamToSrc(stream, localVideo)
   myPeerConnection.addStream(stream)
@@ -54,4 +61,27 @@ const errorStream = error => {
 const sendToServer = msg => {
   msg = JSON.stringify(msg)
   socket.emit('message', msg)
+}
+
+const reportError = error => {
+  console.log(('Error ' + error.name + ': ' + error.message))
+}
+
+socket.on('videoMessage', msg => {
+  switch(msg.type) {
+    case 'video-offer': // Invitation and offer to chat
+        handleVideoOfferMsg(msg)
+        break
+  }
+})
+
+const createPeerConnection = () => {
+  console.log('inside createPeerConnection, setting up connection')
+  // Create an RTCPeerConnection which knows to use our chosen
+  // STUN server.
+  myPeerConnection = new RTCPeerConnection({iceServers: [{'urls': 'stun:' + stunServer}]})
+
+  myPeerConnection.onicecandidate = handleICECandidateEvent
+  myPeerConnection.onaddstream = handleAddStreamEvent // to add remote stream on local remoteVideo element, one a stream is received
+  myPeerConnection.onnegotiationneeded = handleNegotiationEvent
 }
