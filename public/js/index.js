d858f6d914f60ec150e0fcede80db3f1a0cbc5df
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-08 10:23:56 +0530

    Change server .js to http

--- public/js/index.js
+++ public/js/index.js
@@ -23,7 +23,7 @@ let conn = {}
 
 const peerObj = {
   host: '0.0.0.0',
-  port: 8000,
+  port: 3000,
   path: '/peerjs',
   debug: 3,
   config: {icerServers: [
--- server.js
+++ server.js
@@ -1,33 +1,26 @@
-const path = require('path')
-const https = require('https')
 const express = require('express')
-const fs = require('fs')
-const PeerServer = require('peer').PeerServer
-
-const optionsHttps = {
-  key: fs.readFileSync('/mykey.pem'),
-  cert: fs.readFileSync('/my-cert.pem')
-}
-
 const app = express()
-const httpsServer = https.createServer(optionsHttps, app)
-PeerServer({port: process.env.PORT || 8000,
-  key: 'peerjs'})
+const path = require('path')
+const server = require('http').createServer(app)
+const ExpressPeerServer = require('peer').ExpressPeerServer
+const io = require('socket.io').listen(server)
 
-const io = require('socket.io')(httpsServer)
-
-app.use(express.static(path.join(__dirname, 'public')))
-
-httpsServer.listen(3000, (req, res) => {
-  console.log('listening on port 3000')
-})
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-})
+app.use('/peerjs', ExpressPeerServer(server, {
+	debug: true
+}))
 
 const users = []
 const connections = []
+
+server.listen(3000, () => {
+  console.log('server running')
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+
+})
 
 io.sockets.on('connection', socket => {
   connections.push(socket)
@@ -51,7 +44,7 @@ io.sockets.on('connection', socket => {
 
   // New users
   socket.on('new user', (obj, cb) => {
-    console.log('new user', obj)
+		console.log('new user', obj)
     let fusers = users.filter(user => user.name === obj.name)
     if (fusers[0]) {
       cb(false)
@@ -68,7 +61,7 @@ io.sockets.on('connection', socket => {
   })
 
   const updateUsernames = () => {
-    let i = 0
+    var i = 0
     for (; i < users.length; i++) {
       let onlineUserList = []
       users.map(user => {
@@ -80,66 +73,3 @@ io.sockets.on('connection', socket => {
     }
   }
 })
-
-// const express = require('express')
-// const app = express()
-// const fs = require('fs')
-// const https = require('https')
-// const server = require('http').createServer(app)
-// const path = require('path')
-// const ExpressPeerServer = require('peer').ExpressPeerServer
-// const io = require('socket.io').listen(server)
-//
-// const credentials = {
-//   key: fs.readFileSync('/etc/apache2/ssl/apache.key'),
-//   cert: fs.readFileSync('/etc/apache2/ssl/apache.crt')
-// }
-//
-// const httpsServer = https.createServer(credentials, app).listen(3000, (req, res) => {
-// 	console.log('listening on 3000')
-// })
-//
-// const options = {
-//     debug: true
-// }
-//
-// const peerServer = ExpressPeerServer(httpsServer, options)
-// app.use('/peerjs', peerServer)
-
-// const express = require('express')
-// const app = express()
-// const path = require('path')
-// const fs = require('fs')
-// const http = require('http')
-// const https = require('https')
-// const httpServer = http.createServer(app)
-// // const server = require('https').createServer(app)
-// const ExpressPeerServer = require('peer').ExpressPeerServer
-//
-// const server = require('https')
-//
-// const options = {
-// 	key: fs.readFileSync('/etc/apache2/ssl/apache.key'),
-//   cert: fs.readFileSync('/etc/apache2/ssl/apache.crt')
-// }
-//
-// const httpsServer = https.createServer(options, app)
-// const io = require('socket.io').listen(httpsServer)
-//
-// const PeerServer = require('peer').PeerServer
-//
-// const peerServer = PeerServer({
-//   port: 9000,
-//   ssl: {
-//     key: fs.readFileSync('/etc/apache2/ssl/apache.key'),
-//     cert: fs.readFileSync('/etc/apache2/ssl/apache.crt')
-//   }
-// })
-//
-// app.use('/peerjs', ExpressPeerServer(httpsServer, {
-// 	debug: true
-// }))
-//
-// // app.use('/peerjs', ExpressPeerServer(peerServer, {
-// // 	debug: true
-// // }))
